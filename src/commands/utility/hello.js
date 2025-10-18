7502964149af85fe660c97b66aae219833a9bd3a
Peter Cort <petercort@gmail.com>
2025-10-18 19:52:41 -0400

    updating version

--- src/commands/utility/hello.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-
-export const data = new SlashCommandBuilder()
-  .setName('hello')
-  .setDescription('Hello there!!');
-
-export async function execute(interaction) {
-  await interaction.reply(`Hello there ${interaction.user.username}!`);
-}
