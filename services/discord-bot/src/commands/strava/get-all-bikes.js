163999b581d390ce13b54eba828a9020ad3c33d0
Peter Cort <petercort@gmail.com>
2025-10-19 16:31:55 -0400

    big update splitting the app

--- services/discord-bot/src/commands/strava/get-all-bikes.js
+++ services/discord-bot/src/commands/strava/get-all-bikes.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from 'discord.js';
-import { BikesTable, UsersTable } from '../../db-objects.js';
+import { getUser, getUserBikes } from '../../shared-library/backend-api-client.js';
 
 const METERS_TO_MILES_CONVERSION = 0.000621371;
 
@@ -9,22 +9,21 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(interaction) {
   const userId = interaction.user.id;
-  // Look up if the user is in the database
+  
   try {
-    const user = await UsersTable.findOne({ where: { userId } });
+    // Look up if the user is in the database
+    const user = await getUser(userId);
     if (!user) {
       return await interaction.reply({ content: 'Please connect your Strava using the /connect_strava command.', ephemeral: true });
     }
-  } catch (error) {
-    console.error('Error fetching user:', error);
-    return await interaction.reply({ content: 'There was an error querying data, please check back in a bit.', ephemeral: true });
-  }
-  try {
-    // Query the BikesTable to get all bikes for the user
-    const bikes = await BikesTable.findAll({ where: { userId } });
+
+    // Query the backend API to get all bikes for the user
+    const bikes = await getUserBikes(userId);
+    
     if (bikes.length === 0) {
       return await interaction.reply({ content: 'No bikes found! Add a bike by going to https://www.strava.com/settings/gear and adding a bike. Then run /sync_bikes to sync your bikes.', ephemeral: true });
     }
+    
     const bikeList = bikes.map(bike => `${bike.name}: ${bike.brand} ${bike.model}. ${Math.round(bike.distance * METERS_TO_MILES_CONVERSION)} miles. Last waxed on ${bike.lastWaxedDate} at ${Math.round(bike.lastWaxedDistance * METERS_TO_MILES_CONVERSION)} miles.`).join('\n');
     return await interaction.reply({ content: `Your bikes:\n${bikeList}`, ephemeral: true });
   } catch (error) {
--- services/discord-bot/src/commands/strava/get-last-ride.js
+++ services/discord-bot/src/commands/strava/get-last-ride.js
@@ -1,7 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { EmbedBuilder } from 'discord.js';
-import axios from 'axios';
-import { getStravaAuthentication } from '../../shared-library/strava-authentication.js';
+import { getLatestRide } from '../../shared-library/backend-api-client.js';
 
 export const data = new SlashCommandBuilder()
   .setName('get_latest_ride')
@@ -10,21 +9,11 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction) {
   const userId = interaction.user.id;
 
-  // Do some logic here to determine if the access token is expired if so refresh it and then use it
-  const stravaAccessToken = await getStravaAuthentication(userId);
-  if (!stravaAccessToken) {
-    return interaction.reply({ content: 'You need to connect your Strava account first.', ephemeral: true });
-  }
   try {
-    const response = await axios.get('https://www.strava.com/api/v3/athlete/activities', {
-      headers: { Authorization: `Bearer ${stravaAccessToken}` },
-      params: { per_page: 1 }
-    });
-
-    const latestRide = response.data[0];
-
+    const latestRide = await getLatestRide(userId);
+    
     if (!latestRide) {
-      return interaction.reply({ content: 'No rides found.', ephemeral: true });
+      return interaction.reply({ content: 'No rides found or you need to connect your Strava account first.', ephemeral: true });
     }
 
     const embed = new EmbedBuilder()
